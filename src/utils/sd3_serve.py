876d9cecb28b9b9f894dfc47782b6655a703e15c
bdambrosio <bruce.dambrosio@gmail.com>
2024-12-14 10:21:58 -0800

    lanuch configs for Owl, chat_search, and sd3_server

--- src/utils/sd3_serve.py
+++ src/utils/sd3_serve.py
@@ -1,49 +1,31 @@
 from fastapi import FastAPI, Response
-from diffusers import DiffusionPipeline
 import torch
-from diffusers import AutoPipelineForText2Image
+from diffusers import StableDiffusion3Pipeline
+
+pipe = StableDiffusion3Pipeline.from_pretrained(
+    "stabilityai/stable-diffusion-3.5-medium", torch_dtype=torch.bfloat16
+).to("cuda")
+
+#pipe = StableDiffusion3Pipeline.from_pretrained(
+#    "stabilityai/stable-diffusion-3.5-large", torch_dtype=torch.bfloat16
+#).to("cuda")
 from PIL import Image
 from io import BytesIO
 
 app = FastAPI()
 
-import torch
-from diffusers import StableDiffusion3Pipeline, SD3Transformer2DModel, FlashFlowMatchEulerDiscreteScheduler
-from peft import PeftModel
-
-# Load LoRA
-transformer = SD3Transformer2DModel.from_pretrained(
-    "stabilityai/stable-diffusion-3-medium-diffusers",
-    subfolder="transformer",
-    torch_dtype=torch.float16,
-)
-transformer = PeftModel.from_pretrained(transformer, "jasperai/flash-sd3")
-
-
-# Pipeline
-pipe = StableDiffusion3Pipeline.from_pretrained(
-    "stabilityai/stable-diffusion-3-medium-diffusers",
-    transformer=transformer,
-    torch_dtype=torch.float16,
-    text_encoder_3=None,
-    tokenizer_3=None
-)
-
-# Scheduler
-pipe.scheduler = FlashFlowMatchEulerDiscreteScheduler.from_pretrained(
-  "stabilityai/stable-diffusion-3-medium-diffusers",
-  subfolder="scheduler",
-)
-
 pipe.to("cuda")
 
 prompt = "A raccoon trapped inside a glass jar full of colorful candies, the background is steamy with vivid colors."
 
-image = pipe(prompt, num_inference_steps=4, guidance_scale=0).images[0]
 @app.get("/generate_image")
 async def generate_image(prompt: str, size: str = "256X192"):
     #pipeline("An image of a squirrel in Picasso style").images[0]
-    image = pipe(prompt=prompt, num_inference_steps=40, guidance_scale=0.0).images[0]
+    image = pipe(prompt=prompt, 
+                 num_inference_steps=40,
+                 height=512,
+                 width=512,
+                 guidance_scale=4.00).images[0]
     
     # Convert the image to bytes
     img_buffer = BytesIO()
