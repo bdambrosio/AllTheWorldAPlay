85389efdca8167df4b34e042f4ad07b1f1b9cefb
bdambrosio <bruce.dambrosio@gmail.com>
2025-05-09 09:06:20 -0700

    openned up CORS in server

--- src/sim/main.py
+++ src/sim/main.py
@@ -51,8 +51,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",  # React dev server
-                   "http://129.80.23.144:3000"],  # React dev server
+    allow_origins=["*"],  # React dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
