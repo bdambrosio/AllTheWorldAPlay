2a3c9750648c2350a3fdd1f03f0f2b8bab03fcec
bdambrosio <bruce.dambrosio@gmail.com>
2025-05-09 13:39:08 -0700

    fix for runpod - always use https

--- src/sim/webworld/src/config.js
+++ src/sim/webworld/src/config.js
@@ -1,7 +1,7 @@
 const config = {
     // Use the current hostname for the server URL
-    serverUrl: window.location.hostname === 'localhost' 
-        ? 'http://localhost:8000'
+    serverUrl: window.location.protocol === 'https:' 
+        ? `https://${window.location.hostname}:8000`
         : `http://${window.location.hostname}:8000`
 };
 
