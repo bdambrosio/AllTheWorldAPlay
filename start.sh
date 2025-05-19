cf8b6fbf9a9cd539014dd98cad894a5ca6516f1b
bdambrosio <bruce.dambrosio@gmail.com>
2025-05-19 14:37:44 -0700

    minor tweaks for Docker

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Start the Python backend
 cd /app/src/sim/
-uvicorn main:app --host 0.0.0.0 --port 8000 &      
+uvicorn main:app --host 0.0.0.0 --port 8000  &      
 cd /app/src/utils/    # main engine
-fastapi run lcmLora-serve.py --host 0.0.0.0 --port 5008 &    # image thumbs
+fastapi run lcmLora-serve.py --host 0.0.0.0 --port 5008  &    # image thumbs
 #fastapi run blank_image_serve.py --host 0.0.0.0 --port 5008 &    # image thumbs
 cd /app/src/sim/webworld
 npm start &                                                    # UI
