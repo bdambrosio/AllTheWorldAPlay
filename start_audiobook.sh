cc8649bb0ebbc06bf3815a5e91c29086a1246425
bdambrosio <bruce.dambrosio@gmail.com>
2025-06-07 09:56:30 -0700

    updated Dockerfile.audiobook

--- start_audiobook.sh
+++ start_audiobook.sh
@@ -3,5 +3,6 @@
 cd /app/src/sim/
 uvicorn replay:app --host 0.0.0.0 --port 8000 &      
 cd /app/src/sim/audiobook
+npm install
 npm start &                                                    # UI
 wait -n                                     # exit if any child dies
