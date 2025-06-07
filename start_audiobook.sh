2a52453e4fbb2c11913492da476cc85a74ab67f6
bdambrosio <bruce.dambrosio@gmail.com>
2025-06-07 09:36:11 -0700

    dockerfile for audiobook

---
+++ start_audiobook.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Start the Python backend
+cd /app/src/sim/
+uvicorn replay:app --host 0.0.0.0 --port 8000 &      
+cd /app/src/sim/audiobook
+npm start &                                                    # UI
+wait -n                                     # exit if any child dies
